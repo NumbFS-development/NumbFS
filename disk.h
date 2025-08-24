c9d685d691fdc574b334d2a09ccb3424203a1e92
Hongzhen Luo <hongzhen_lhz@163.com>
2025-08-24 21:44:24 +0800

    numbfs: add disk layout header file
    This commit introduces the on-disk structure definitions for the NumbFS
    filesystem.
    
    Signed-off-by: Hongzhen Luo <hongzhen_lhz@163.com>

---
+++ disk.h
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025, Hongzhen Luo
+ */
+
+/*
+ * numbfs disk layout definitions
+ *
+ * This header defines the on-disk structures and constants for the NUMBFS filesystem.
+ * It includes:
+ * - Magic number and basic constants (block size, root inode, etc.)
+ * - Superblock structure (filesystem metadata and bitmaps location)
+ * - Inode structure (file metadata and data block pointers)
+ * - Directory entry structure (file name and inode number mapping)
+ * - Extended attribute entry structure (key-value storage)
+ * - Compile-time checks for structure sizes
+ *
+ * All structures are designed to be packed and aligned to avoid padding.
+ * The superblock is located at block offset 1 (after the reserved boot block).
+ */
+
+#ifndef __NUMBFS_DISK_H
+#define __NUMBFS_DISK_H
+
+#include <linux/types.h>
+#include <linux/build_bug.h>
+
+#define NUMBFS_MAGIC    0x4E554D42 /* "NUMB" */
+
+#define NUMBFS_BYTES_PER_BLOCK 512
+
+/* the first block is reserved */
+#define NUMBFS_SUPER_OFFSET NUMBFS_BYTES_PER_BLOCK
+
+#define NUMBFS_HOLE	(-32)
+
+/* root inode number */
+#define NUMBFS_ROOT_NID	0
+
+#define NUMBFS_NUM_DATA_ENTRY	10
+#define NUMBFS_MAX_PATH_LEN	60
+#define NUMBFS_MAX_ATTR 32
+
+/* 128-byte on-disk numbfs superblock, 64 bytes should be enough, but... */
+struct numbfs_super_block {
+	__le32 s_magic;
+	/* feature bits */
+	__le32 s_feature;
+	/* block addr of inode bitmap */
+	__le32 s_ibitmap_start;
+	/* block addr of inode zone */
+	__le32 s_inode_start;
+	/* block addr of block bitmap */
+	__le32 s_bbitmap_start;
+	/* block addr of data start */
+	__le32 s_data_start;
+	/* total num of inodes */
+	__le32 s_total_inodes;
+	/* num of free inodes */
+	__le32 s_free_inodes;
+	/* num of total data blocks */
+	__le32 s_data_blocks;
+	/* num of free data blocks */
+	__le32 s_free_blocks;
+	/* reserved */
+	__u8 s_reserved[88];
+};
+
+/* 64-byte on-disk numbfs inode */
+struct numbfs_inode {
+	__le16 i_ino;
+	__le16 i_nlink;
+	__le16 i_uid;
+	__le16 i_gid;
+	__le32 i_mode;
+	__le32 i_size;
+	/* start block addr of xattrs */
+	__le32 i_xattr_start;
+	/* number of xattrs */
+	__u8 i_xattr_count;
+	__u8 reserved2[3]; /* padding */
+	/* block addr of data blocks */
+	__le32 i_data[10];
+};
+
+/* 64-byte on-disk numbfs dirent */
+struct numbfs_dirent {
+	__u8 name_len;
+	__u8 type;
+	char name[NUMBFS_MAX_PATH_LEN];
+	__le16 ino;
+};
+
+/* on-disk xattr entry */
+struct numbfs_xattr_entry {
+	__u8 e_valid;
+	__le16 e_name_len;
+	__le16 e_value_len;
+	__le16 e_start;
+};
+
+/* check the on-disk layout at compile time */
+static inline void numbfs_check_ondisk(void)
+{
+        BUILD_BUG_ON(sizeof(struct numbfs_super_block) != 128);
+        BUILD_BUG_ON(sizeof(struct numbfs_inode) != 64);
+        BUILD_BUG_ON(sizeof(struct numbfs_dirent) != 64);
+}
+
+#endif

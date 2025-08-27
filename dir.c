NumbFS-development/NumbFS

commit cf9ae1e835ba0724d9a2eb36f73c354dc56a6dba

numbfs: implement directory operations and block allocation
This commit adds comprehensive directory operations and block/inode
allocation management to NumbFS, significantly expanding the filesystem's
functionality.

The filesystem now supports basic directory operations including listing
and lookup, as well as block and inode allocation.

Signed-off-by: Hongzhen Luo <hongzhen_lhz@163.com>

dir.c
@@ -1,0 +1,133 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025, Hongzhen Luo
+ */
+
+#include "internal.h"
+#include <linux/pagemap.h>
+#include <linux/iomap.h>
+
+void numbfs_dir_set_ops(struct inode *inode)
+{
+        inode->i_op = &numbfs_dir_iops;
+        inode->i_fop = &numbfs_dir_fops;
+        inode->i_mapping->a_ops = &numbfs_aops;
+}
+
+/* find the target nid according to the name */
+static int numbfs_inode_by_name(struct inode *dir, const char *name,
+                                int namelen, int *nid, int *offset)
+{
+        struct numbfs_dirent *de;
+        struct numbfs_buf buf;
+        int i, ret, err;
+
+        ret = -ENOENT;
+        for (i = 0; i < dir->i_size; i += sizeof(*de)) {
+                if (i % NUMBFS_BYTES_PER_BLOCK == 0) {
+                        if (i > 0)
+                                numbfs_put_buf(&buf);
+
+                        numbfs_init_buf(&buf, dir, i / NUMBFS_BYTES_PER_BLOCK);
+                        err = numbfs_read_buf(&buf);
+                        if (err)
+                                return err;
+                }
+                de = (struct numbfs_dirent*)((unsigned char*)buf.base +
+                                (i % NUMBFS_BYTES_PER_BLOCK));
+                if (de->name_len == namelen &&
+                    !memcmp(name, de->name, namelen)) {
+                        ret = 0;
+                        *nid = le16_to_cpu(de->ino);
+                        if (offset)
+                                *offset = i;
+                        break;
+                }
+
+        }
+
+        numbfs_put_buf(&buf);
+        return ret;
+}
+
+static int numbfs_readdir(struct file *file, struct dir_context *ctx)
+{
+        struct inode *dir = file_inode(file);
+        size_t dirsize = i_size_read(dir);
+        struct numbfs_buf buf;
+        struct numbfs_dirent *de;
+        int err;
+
+        while (ctx->pos < dirsize) {
+                const char *de_name;
+                unsigned int de_namelen;
+                unsigned char de_type;
+
+                if (ctx->pos % NUMBFS_BYTES_PER_BLOCK == 0) {
+                        if (ctx->pos > 0)
+                                numbfs_put_buf(&buf);
+                        numbfs_init_buf(&buf, dir, ctx->pos / NUMBFS_BYTES_PER_BLOCK);
+                        err = numbfs_read_buf(&buf);
+                        if (err) {
+                                pr_info("numbfs: error to read dir block@%lld, err: %d\n", ctx->pos / NUMBFS_BYTES_PER_BLOCK, err);
+                                goto out;
+                        }
+
+                }
+
+                de = (struct numbfs_dirent*)((unsigned char*)buf.base +
+                                (ctx->pos % NUMBFS_BYTES_PER_BLOCK));
+                de_name = de->name;
+                de_namelen = de->name_len;
+                de_type = de->type;
+                ctx->pos += sizeof(struct numbfs_dirent);
+                if (!de_namelen) {
+                        pr_err("numbfs: invalid dirent: namelen=0\n");
+                        err = -EINVAL;
+                        goto out;
+                }
+
+                if (!dir_emit(ctx, de_name, de_namelen, le16_to_cpu(de->ino),
+                              de_type)) {
+			err = 0;
+			goto out;
+                }
+        }
+out:
+        numbfs_put_buf(&buf);
+        return err;
+}
+
+static struct dentry *numbfs_dir_lookup(struct inode *dir,
+                struct dentry *dentry, unsigned int flags)
+{
+        int res, ino;
+        struct inode *inode;
+
+        if (dentry->d_name.len > NUMBFS_MAX_PATH_LEN)
+                return ERR_PTR(-ENAMETOOLONG);
+
+        res = numbfs_inode_by_name(dir, dentry->d_name.name,
+                                dentry->d_name.len, &ino, NULL);
+        if (res) {
+                if (res != -ENOENT)
+                        return ERR_PTR(res);
+                inode = NULL;
+        } else {
+                inode = numbfs_iget(dir->i_sb, ino);
+                if (IS_ERR(inode))
+                        return ERR_PTR(PTR_ERR(inode));
+        }
+
+        return d_splice_alias(inode, dentry);
+}
+
+const struct inode_operations numbfs_dir_iops = {
+        .lookup         = numbfs_dir_lookup,
+};
+
+const struct file_operations numbfs_dir_fops = {
+        .llseek         = generic_file_llseek,
+        .read           = generic_read_dir,
+        .iterate_shared = numbfs_readdir,
+};

Makefile
@@ -3,4 +3,4 @@
 #
 obj-m += numbfs.o
 
-numbfs-objs := super.o inode.o utils.o
+numbfs-objs := super.o inode.o utils.o dir.o data.o

data.c
@@ -1,0 +1,60 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025, Hongzhen Luo
+ */
+
+#include "internal.h"
+#include <linux/buffer_head.h>
+#include <linux/mpage.h>
+#include <linux/iomap.h>
+
+#define NUMBFS_READ     0
+#define NUMBFS_WRITE    1
+
+static int numbfs_iomap(struct inode *inode, loff_t offset, loff_t length,
+                        struct iomap *iomap, int type)
+{
+        struct numbfs_inode_info *ni = NUMBFS_I(inode);
+        int blk;
+
+        blk = numbfs_iaddrspace_blkaddr(ni, offset, type == NUMBFS_WRITE);
+        if (blk < 0 && blk != NUMBFS_HOLE)
+                return -EINVAL;
+
+        iomap->flags = 0;
+        iomap->offset = (offset >> NUMBFS_BLOCK_BITS) << NUMBFS_BLOCK_BITS;
+        iomap->bdev = inode->i_sb->s_bdev;
+        iomap->length = 1 << NUMBFS_BLOCK_BITS;
+        iomap->private = NULL;
+
+        if (blk == NUMBFS_HOLE) {
+                iomap->type = IOMAP_HOLE;
+                iomap->addr = IOMAP_NULL_ADDR;
+                iomap->flags |= IOMAP_F_NEW;
+                return 0;
+        }
+
+        iomap->type = IOMAP_MAPPED;
+        iomap->addr = numbfs_data_blk(ni->sbi, blk) << NUMBFS_BLOCK_BITS;
+        return 0;
+}
+
+static int numbfs_iomap_read_begin(struct inode *inode, loff_t offset,
+                loff_t length, unsigned int flags, struct iomap *iomap,
+                struct iomap *srcmap)
+{
+        return numbfs_iomap(inode, offset, length, iomap, NUMBFS_READ);
+}
+
+const struct iomap_ops numbfs_iomap_read_ops = {
+        .iomap_begin    = numbfs_iomap_read_begin,
+};
+
+static int numbfs_read_folio(struct file *file, struct folio *folio)
+{
+        return iomap_read_folio(folio, &numbfs_iomap_read_ops);
+}
+
+const struct address_space_operations numbfs_aops = {
+        .read_folio             = numbfs_read_folio,
+};

inode.c
@@ -63,7 +63,7 @@ static int numbfs_fill_inode(struct inode *inode)
                 err = -EOPNOTSUPP;
                 break;
         case S_IFDIR:
-                // TODO: XXX
+                numbfs_dir_set_ops(inode);
                 err = 0;
                 break;
         default:

utils.c
@@ -77,3 +77,173 @@ struct numbfs_inode *numbfs_idisk(struct numbfs_buf *buf,
                         (nid % NUMBFS_NODES_PER_BLOCK);
         return ret;
 }
+
+/**
+ * numbfs_iaddrspace_blkaddr - Get or allocate a block address for a given file position
+ * @ni: Pointer to the numbfs inode info structure
+ * @pos: File position (byte offset) to map to a block
+ * @alloc: If true, allocate a new block if the position is a hole
+ *
+ * Return:
+ *   On success, returns the physical block number (>= 0).
+ *   If @pos is out of range, returns -E2BIG.
+ *   If block allocation fails (when @alloc is true), returns the error code
+ *   from numbfs_balloc().
+ */
+int numbfs_iaddrspace_blkaddr(struct numbfs_inode_info *ni,
+                              unsigned long pos, bool alloc)
+{
+        int blk, err;
+
+        if ((pos / NUMBFS_BYTES_PER_BLOCK) >= NUMBFS_NUM_DATA_ENTRY) {
+                pr_err("numbfs: pos@%ld is out of range\n", pos);
+                return -E2BIG;
+        }
+
+        blk = ni->data[pos / NUMBFS_BYTES_PER_BLOCK];
+        if (alloc && blk == NUMBFS_HOLE) {
+                err = numbfs_balloc(ni->vfs_inode.i_sb, &blk);
+                if (err)
+                        return err;
+                ni->data[pos / NUMBFS_BYTES_PER_BLOCK] = blk;
+        }
+        return blk;
+}
+
+static int numbfs_bitmap_alloc(struct super_block *sb, int startblk,
+                               int total, int *res, int *quota)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+        int err, i, byte, bit;
+        struct numbfs_buf buf;
+        unsigned char *bitmap;
+
+        err = -ENOMEM;
+        *res = -1;
+        mutex_lock(&sbi->s_mutex);
+        /* run out of quota */
+        if (!*quota)
+                goto out;
+        for (i = 0; i < total; i++) {
+                if (i % NUMBFS_BLOCKS_PER_BLOCK == 0) {
+                        if (i > 0) {
+                                numbfs_commit_buf(&buf);
+                                numbfs_put_buf(&buf);
+                        }
+                        numbfs_init_buf(&buf, sb->s_bdev->bd_inode, numbfs_bmap_blk(startblk, i));
+                        err = numbfs_read_buf(&buf);
+                        if (err) {
+                                pr_err("numbfs: failed to read bitmap block@%d\n", buf.blkaddr);
+                                goto out;
+                        }
+                        bitmap = (unsigned char*)buf.base +
+                                ((buf.blkaddr << NUMBFS_BLOCK_BITS) & (PAGE_SIZE - 1));
+                }
+
+
+                byte = numbfs_bmap_byte(i);
+                bit = numbfs_bmap_bit(i);
+                if (!(bitmap[byte] & (1 << bit))) {
+                        err = 0;
+                        *res = i;
+                        /* mark this folio dirty */
+                        folio_lock(buf.folio);
+                        bitmap[byte] |= (1 << bit);
+                        filemap_dirty_folio(folio_inode(buf.folio)->i_mapping,
+                                            buf.folio);
+                        folio_unlock(buf.folio);
+                        break;
+                }
+
+        }
+        if (!err)
+                *quota -= 1;;
+out:
+        mutex_unlock(&sbi->s_mutex);
+        numbfs_put_buf(&buf);
+        return err;
+}
+
+static int numbfs_bitmap_free(struct super_block *sb, int startblk, int free,
+                              int *quota)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+        int err, byte, bit;
+        struct numbfs_buf buf;
+        unsigned char *bitmap;
+
+        mutex_lock(&sbi->s_mutex);
+        numbfs_init_buf(&buf, sb->s_bdev->bd_inode,
+                        numbfs_bmap_blk(startblk, free));
+        err = numbfs_read_buf(&buf);
+        if (err)
+                goto out;
+
+        bitmap = (unsigned char*)buf.base +
+                ((buf.blkaddr << NUMBFS_BLOCK_BITS) & (PAGE_SIZE - 1));
+        byte = numbfs_bmap_byte(free);
+        bit = numbfs_bmap_bit(free);
+        WARN_ON(!(bitmap[byte] & (1 << bit)));
+        /* mark this folio dirty */
+        folio_lock(buf.folio);
+        bitmap[byte] &= ~(1 << bit);
+        filemap_dirty_folio(folio_inode(buf.folio)->i_mapping,
+                            buf.folio);
+        folio_unlock(buf.folio);
+        *quota += 1;
+        err = 0;
+out:
+        mutex_unlock(&sbi->s_mutex);
+        numbfs_put_buf(&buf);
+        return err;
+}
+
+int numbfs_balloc(struct super_block *sb, int *blk)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+        int res, err;
+
+        err = numbfs_bitmap_alloc(sb, sbi->bbitmap_start, sbi->data_blocks,
+                                  &res, &sbi->free_blocks);
+        if (err)
+                return err;
+
+        *blk = res;
+        return 0;
+}
+
+int numbfs_bfree(struct super_block *sb, int blk)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+
+        if (blk >= sbi->data_blocks)
+                return -EINVAL;
+
+        return numbfs_bitmap_free(sb, sbi->bbitmap_start, blk,
+                                  &sbi->free_blocks);
+}
+
+int numbfs_ialloc(struct super_block *sb, int *nid)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+        int res, err;
+
+        err = numbfs_bitmap_alloc(sb, sbi->ibitmap_start, sbi->total_inodes,
+                                &res, &sbi->free_inodes);
+        if (err)
+                return err;
+
+        *nid = res;
+        return 0;
+}
+
+int numbfs_ifree(struct super_block *sb, int nid)
+{
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(sb);
+
+        if (nid >= sbi->total_inodes)
+                return -EINVAL;
+
+        return numbfs_bitmap_free(sb, sbi->ibitmap_start, nid,
+                                  &sbi->free_inodes);
+}

commit 1cd51a38901f0de5510e2c8d58991037dea3b2ac

numbfs: fix directory entry pointer calculation in multi-block folios
Adjust directory entry pointer calculation to properly handle multi-block
folios by accounting for block offset within the folio. The previous code
assumed each folio contained only one block, which caused incorrect pointer
calculations when folios contained multiple blocks.

Fixes: cf9ae1e835ba ("numbfs: implement directory operations and block allocation")
Signed-off-by: Hongzhen Luo <hongzhen_lhz@163.com>

dir.c
@@ -25,7 +25,7 @@ static int numbfs_inode_by_name(struct inode *dir, const char *name,
 {
         struct numbfs_dirent *de;
         struct numbfs_buf buf;
-        int i, ret, err;
+        int i, ret, err, off;
 
         ret = -ENOENT;
         for (i = 0; i < dir->i_size; i += sizeof(*de)) {
@@ -38,7 +38,9 @@ static int numbfs_inode_by_name(struct inode *dir, const char *name,
                         if (err)
                                 return err;
                 }
+                off = (i >> NUMBFS_BLOCK_BITS) << NUMBFS_BLOCK_BITS;
                 de = (struct numbfs_dirent*)((unsigned char*)buf.base +
+                                (off & (folio_size(buf.folio) - 1)) +
                                 (i % NUMBFS_BYTES_PER_BLOCK));
                 if (de->name_len == namelen &&
                     !memcmp(name, de->name, namelen)) {
@@ -61,7 +63,7 @@ static int numbfs_readdir(struct file *file, struct dir_context *ctx)
         size_t dirsize = i_size_read(dir);
         struct numbfs_buf buf;
         struct numbfs_dirent *de;
-        int err;
+        int err, off;
 
         while (ctx->pos < dirsize) {
                 const char *de_name;
@@ -80,7 +82,9 @@ static int numbfs_readdir(struct file *file, struct dir_context *ctx)
 
                 }
 
+                off = (ctx->pos >> NUMBFS_BLOCK_BITS) << NUMBFS_BLOCK_BITS;
                 de = (struct numbfs_dirent*)((unsigned char*)buf.base +
+                                (off & (folio_size(buf.folio) - 1)) +
                                 (ctx->pos % NUMBFS_BYTES_PER_BLOCK));
                 de_name = de->name;
                 de_namelen = de->name_len;
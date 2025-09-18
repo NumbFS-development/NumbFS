NumbFS-development/NumbFS

commit 33e6a68e0fff9589be87893097a840a3f7ed7401

numbfs: implement persistent timestamp support
This commit adds comprehensive timestamp functionality to the NumbFS
filesystem by introducing on-disk timestamp storage, proper inode
initialization with current time, and full getattr/setattr support.

Signed-off-by: Hongzhen Luo <hongzhen_lhz@163.com>

dir.c
@@ -136,7 +136,8 @@ static void numbfs_dir_init_inode(struct inode *inode, struct inode *dir,
                                   int nid, umode_t mode)
 {
         struct numbfs_inode_info *ni = NUMBFS_I(inode);
-        int i;
+        struct timespec64 now = current_time(inode);
+        int i, blk;
 
         inode->i_ino = nid;
         inode->i_mode = mode;
@@ -145,6 +146,10 @@ static void numbfs_dir_init_inode(struct inode *inode, struct inode *dir,
         set_nlink(inode, S_ISDIR(mode) ? 2 : 1);
         inode->i_size = 0;
         inode->i_blocks = 0;
+
+        inode_set_atime(inode, now.tv_sec, now.tv_nsec);
+        inode_set_ctime(inode, now.tv_sec, now.tv_nsec);
+        inode_set_mtime(inode, now.tv_sec, now.tv_nsec);
 
         if (S_ISDIR(mode))
                 numbfs_dir_set_ops(inode);
@@ -155,6 +160,10 @@ static void numbfs_dir_init_inode(struct inode *inode, struct inode *dir,
         ni->nid = nid;
         for (i = 0; i < NUMBFS_NUM_DATA_ENTRY; i++)
                 ni->data[i] = NUMBFS_HOLE;
+
+        blk = -1;
+        (void)numbfs_balloc(inode->i_sb, &blk);
+        ni->xattr_start = blk;
 }
 
 /* return a locked new inode */

disk.h
@@ -91,6 +91,13 @@ struct numbfs_dirent {
 	__le16 ino;
 };
 
+struct numbfs_timestamps {
+	__le64 t_atime;
+	__le64 t_mtime;
+	__le64 t_ctime;
+	__u8 reserved[8];
+};
+
 /* on-disk xattr entry */
 struct numbfs_xattr_entry {
 	__u8 e_valid;
@@ -105,6 +112,7 @@ static inline void numbfs_check_ondisk(void)
         BUILD_BUG_ON(sizeof(struct numbfs_super_block) != 128);
         BUILD_BUG_ON(sizeof(struct numbfs_inode) != 64);
         BUILD_BUG_ON(sizeof(struct numbfs_dirent) != 64);
+        BUILD_BUG_ON(sizeof(struct numbfs_timestamps) != 32);
 }
 
 #endif

inode.c
@@ -38,6 +38,33 @@ void numbfs_setsize(struct inode *inode, loff_t newsize)
 	filemap_invalidate_unlock(inode->i_mapping);
 }
 
+static int numbfs_set_timestamps(struct inode *inode)
+{
+        struct numbfs_inode_info *ni = NUMBFS_I(inode);
+        struct numbfs_timestamps *nt;
+        struct numbfs_buf buf;
+        int err;
+
+        err = numbfs_binit(&buf, inode->i_sb->s_bdev,
+                           numbfs_data_blk(ni->sbi, ni->xattr_start));
+        if (err)
+                return err;
+
+        err = numbfs_brw(&buf, NUMBFS_READ);
+        if (err) {
+                numbfs_bput(&buf);
+                return err;
+        }
+
+        nt = (struct numbfs_timestamps*)buf.base;
+        (void)inode_set_atime(inode, (time64_t)le64_to_cpu(nt->t_atime), 0);
+        (void)inode_set_mtime(inode, (time64_t)le64_to_cpu(nt->t_mtime), 0);
+        (void)inode_set_ctime(inode, (time64_t)le64_to_cpu(nt->t_ctime), 0);
+
+        numbfs_bput(&buf);
+        return 0;
+}
+
 static int numbfs_fill_inode(struct inode *inode)
 {
         struct super_block *sb = inode->i_sb;
@@ -64,8 +91,12 @@ static int numbfs_fill_inode(struct inode *inode)
         ni->nid = inode->i_ino;
         for (i = 0; i < NUMBFS_NUM_DATA_ENTRY; i++)
                 ni->data[i] = le32_to_cpu(di->i_data[i]);
-
+        ni->xattr_start = le32_to_cpu(di->i_xattr_start);
         numbfs_ibuf_put(&buf);
+
+        err = numbfs_set_timestamps(inode);
+        if (err)
+                return err;
 
         err = 0;
         switch(inode->i_mode & S_IFMT) {
@@ -125,7 +156,39 @@ struct inode *numbfs_iget(struct super_block *sb, int nid)
         return inode;
 }
 
+static int numbfs_getattr(struct mnt_idmap *idmap, const struct path *path,
+		          struct kstat *stat, u32 request_mask,
+		          unsigned int query_flags)
+{
+        struct inode *const inode = d_inode(path->dentry);
+
+        generic_fillattr(idmap, request_mask, inode, stat);
+        return 0;
+}
+
+static int numbfs_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
+                          struct iattr *iattr)
+{
+        struct inode *inode = d_inode(dentry);
+        int err;
+
+        err = setattr_prepare(&nop_mnt_idmap, dentry, iattr);
+        if (err)
+                return err;
+
+        if (iattr->ia_valid & ATTR_SIZE && iattr->ia_size != inode->i_size)
+                numbfs_setsize(inode, iattr->ia_size);
+
+        setattr_copy(&nop_mnt_idmap, inode, iattr);
+        mark_inode_dirty(inode);
+
+        return err;
+}
+
+
 const struct inode_operations numbfs_generic_iops = {
+        .getattr        = numbfs_getattr,
+        .setattr        = numbfs_setattr,
 };
 
 static void numbfs_link_free(void *target)
@@ -159,5 +222,7 @@ static const char *numbfs_get_link(struct dentry *dentry, struct inode *inode,
 }
 
 const struct inode_operations numbfs_symlink_iops = {
-        .get_link       = numbfs_get_link
+        .get_link       = numbfs_get_link,
+        .getattr        = numbfs_getattr,
+        .setattr        = numbfs_setattr,
 };

internal.h
@@ -67,6 +67,7 @@ struct numbfs_buf {
 struct numbfs_inode_info {
 	int nid;
 	int data[NUMBFS_NUM_DATA_ENTRY];
+	int xattr_start;
 	struct numbfs_superblock_info *sbi;
 	struct inode vfs_inode;
 };

super.c
@@ -96,6 +96,38 @@ static void numbfs_dump_inode(struct inode *inode, struct numbfs_inode *di)
         di->i_size      = cpu_to_le32(inode->i_size);
         for (i = 0; i < NUMBFS_NUM_DATA_ENTRY; i++)
                 di->i_data[i] = cpu_to_le32(ni->data[i]);
+        di->i_xattr_start = cpu_to_le32(ni->xattr_start);
+}
+
+static int numbfs_dump_timestamps(struct inode *inode)
+{
+        struct numbfs_inode_info *ni = NUMBFS_I(inode);
+        struct numbfs_timestamps *nt;
+        struct numbfs_buf buf;
+        int err;
+
+        err = numbfs_binit(&buf, inode->i_sb->s_bdev,
+                           numbfs_data_blk(ni->sbi, ni->xattr_start));
+        if (err)
+                return err;
+
+        /*
+         * We have to read first because NumbFS's block size is 512B, which is
+         * smaller than the page size (4K), so we must ensure that the subsequent
+         * blocks in the page match those on the disk.
+         */
+        err = numbfs_brw(&buf, NUMBFS_READ);
+        if (err)
+                return err;
+
+        nt = (struct numbfs_timestamps*)buf.base;
+        nt->t_atime = cpu_to_le64((long)inode_get_atime_sec(inode));
+        nt->t_mtime = cpu_to_le64((long)inode_get_mtime_sec(inode));
+        nt->t_ctime = cpu_to_le64((long)inode_get_ctime_sec(inode));
+
+        err = numbfs_brw(&buf, NUMBFS_WRITE);
+        numbfs_bput(&buf);
+        return err;
 }
 
 static int numbfs_write_inode_meta(struct inode *inode)
@@ -107,15 +139,17 @@ static int numbfs_write_inode_meta(struct inode *inode)
 
         di = numbfs_idisk(&buf, inode->i_sb, nid);
         if (IS_ERR(di)) {
-                err = PTR_ERR(di);
-                goto out;
+                numbfs_bput(&buf);
+                return PTR_ERR(di);
         }
 
         numbfs_dump_inode(inode, di);
         err = numbfs_brw(&buf, NUMBFS_WRITE);
-out:
         numbfs_bput(&buf);
-        return err;
+        if (err)
+                return err;
+
+        return numbfs_dump_timestamps(inode);
 }
 
 static int numbfs_write_inode(struct inode *inode, struct writeback_control *wbc)

commit 6468181014865f4620a43ebb219820579aa52d3f

numbfs: fix: initialize xattr block with zeros
Ensure newly allocated extended attribute blocks are properly zeroed
out to prevent reading uninitialized data.

Fixes: 33e6a68e0fff ("numbfs: implement persistent timestamp support")
Signed-off-by: Hongzhen Luo <hongzhen_lhz@163.com>

dir.c
@@ -136,7 +136,9 @@ static void numbfs_dir_init_inode(struct inode *inode, struct inode *dir,
                                   int nid, umode_t mode)
 {
         struct numbfs_inode_info *ni = NUMBFS_I(inode);
+        struct numbfs_superblock_info *sbi = NUMBFS_SB(inode->i_sb);
         struct timespec64 now = current_time(inode);
+        struct numbfs_buf buf;
         int i, blk;
 
         inode->i_ino = nid;
@@ -163,6 +165,13 @@ static void numbfs_dir_init_inode(struct inode *inode, struct inode *dir,
 
         blk = -1;
         (void)numbfs_balloc(inode->i_sb, &blk);
+
+        /* zero out this block */
+        (void)numbfs_binit(&buf, inode->i_sb->s_bdev, numbfs_data_blk(sbi, blk));
+        (void)numbfs_brw(&buf, NUMBFS_READ);
+        memset(buf.base, 0, NUMBFS_BYTES_PER_BLOCK);
+        (void)numbfs_brw(&buf, NUMBFS_WRITE);
+
         ni->xattr_start = blk;
 }
 